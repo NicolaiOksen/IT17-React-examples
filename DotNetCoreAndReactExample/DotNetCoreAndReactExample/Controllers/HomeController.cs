c58b464e3a64af6bc90f39bc3ed62f5d39607533
Nicolai Oksen <nbo@oxygen.dk>
2017-05-02 18:04:32 +0200

    Added extra project with simple setup of React and .Net Core.

---
+++ DotNetCoreAndReactExample/DotNetCoreAndReactExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DotNetCoreAndReactExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("{*url}")]
+        public IActionResult Index()
+        {
+            var test = JsonConvert.DeserializeObject<Dictionary<string,string>>(
+                System.IO.File.ReadAllText("wwwroot/asset-manifest.json"));
+
+            ViewData["css"] = test["main.css"];
+            ViewData["js"] = test["main.js"];
+            return View();
+        }
+    }
+}
---
+++ DotNetCoreAndReactExample/DotNetCoreAndReactExample/Data/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using DotNetCoreAndReactExample.Models;
+using System;
+using System.Linq;
+
+namespace DotNetCoreAndReactExample.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            // Ensures that the database exists
+            context.Database.EnsureCreated();
+            // Look for any students.
+            if (context.Students.Any()) { return; }
+            var students = new Student[]
+            {
+                new Student{
+                    FirstMidName ="Carson",
+                    LastName ="Alexander",
+                    EnrollmentDate =DateTime.Parse("2005-09-01")
+                },
+                new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            };
+            foreach (Student s in students)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+        }
+    }
+}
